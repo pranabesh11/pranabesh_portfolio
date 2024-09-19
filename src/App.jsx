51bc09342138801a04a56808420efcb4edc6b7ba
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2024-09-19 13:29:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>hello</p>
+    </>
+  )
+}
+
+export default App
