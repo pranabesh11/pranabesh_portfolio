f9043fa16a9093eeb683669126f67a742c3a2928
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2024-12-11 12:08:40 +0530

    toast message error solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css';
+import {ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
